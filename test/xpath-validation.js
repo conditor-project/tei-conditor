3361fc7832cc29de39233bb1268ef8ccf3a703fb
Rémy MEJA <remymeja@free.fr>
2018-10-05 07:54:12 +0200

    update unit test

--- test/xpath-validation.js
+++ test/xpath-validation.js
@@ -1,47 +1,57 @@
 'use strict';
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions */
+const { getStylesheetFromSync } = require('../index.js');
 const xpath = require('xpath');
 const DOM = require('xmldom').DOMParser;
 const path = require('path');
 const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 const metadataXpaths = require('co-config/metadata-xpaths.json');
 const Computron = require('computron');
 Promise.promisifyAll(Computron.prototype);
 
 describe('Xpath validation for Conditor load chaining', function () {
-  let xmlDoc;
-
-  before(function () {
-    const transformer = new Computron();
-    return transformer.loadStylesheetAsync(path.resolve('src/source/pubmed/Pubmed2Conditor_TEI1.xsl'))
-      .then(() => transformer.applyAsync(path.join(__dirname, 'dataset/pubmed/pubmed-11748933.xml')))
-      .then(xml => new DOM().parseFromString(xml))
-      .then(doc => {
-        xmlDoc = doc;
-      })
-      .catch(console.error);
-  });
-
-  metadataXpaths.map(metadataXpath => {
-    it(`it should retriving ${metadataXpath.name}`, function () {
-      const evaluator = xpath.parse(metadataXpath.path);
-      const evaluatorOptions = {
-        node: xmlDoc,
-        namespaces: {
-          'TEI': 'http://www.tei-c.org/ns/1.0',
-          'xml': 'http://www.w3.org/XML/1998/namespace'
-        },
-        functions: {
-          'lower-case': function (context, arg) {
-            return context
-              .contextNode
-              .getAttribute('type')
-              .toLowerCase();
-          }
-        }
+  const datasets = fs.readdirSync(path.join(__dirname, 'dataset/pubmed'))
+    .map(dataset => {
+      return {
+        name: dataset,
+        path: path.join(__dirname, 'dataset/pubmed', dataset)
       };
-      evaluator.evaluate(evaluatorOptions);
+    });
+  const stylesheets = getStylesheetFromSync('pubmed');
+
+  stylesheets.map(stylesheet => {
+    datasets.map(dataset => {
+      describe(`${stylesheet.name} with ${dataset.name}`, function () {
+        metadataXpaths.map(metadataXpath => {
+          it(`it should retriving ${metadataXpath.name}`, function () {
+            const transformer = new Computron();
+            return transformer.loadStylesheetAsync(stylesheet.path)
+              .then(() => transformer.applyAsync(dataset.path))
+              .then(xml => new DOM().parseFromString(xml))
+              .then(xmlDoc => {
+                const evaluator = xpath.parse(metadataXpath.path);
+                const evaluatorOptions = {
+                  node: xmlDoc,
+                  namespaces: {
+                    'TEI': 'http://www.tei-c.org/ns/1.0',
+                    'xml': 'http://www.w3.org/XML/1998/namespace'
+                  },
+                  functions: {
+                    'lower-case': function (context, arg) {
+                      return context
+                        .contextNode
+                        .getAttribute('type')
+                        .toLowerCase();
+                    }
+                  }
+                };
+                evaluator.evaluate(evaluatorOptions);
+              });
+          });
+        });
+      });
     });
   });
 });
